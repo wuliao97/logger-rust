381e98124225398d699286b694583ce428988f7b
wuliao97 <wuliao.lw@gmail.com>
2023-09-07 08:26:12 +0900

    fixed a some mistake

---
+++ readme.md
@@ -0,0 +1,13 @@
+# Logger for Rust
+
+# Usage
+```rust
+fn main() {
+    info!("Hello World");
+    warn!("Hello World");
+    error!("Hello World");
+}
+```
+
+# Output
+[[]]
--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,14 @@
+#[macro_export]
 macro_rules! info {
     ($target:expr) => (println!("\x1b[38;5;004m{}\x1b[m", $target))
 }
 
+#[macro_export]
 macro_rules! warn {
     ($target:expr) => (println!("\x1b[38;5;220m{}\x1b[m", $target))
 }
 
+#[macro_export]
 macro_rules! error {
     ($target:expr) => (println!("\x1b[38;5;124m{}\x1b[m", $target))
 }
@@ -22,13 +25,13 @@ mod tests {
         error!("Hello World");
     }
 
-    #[test]
-    fn main() {
-        for color in 0..256 {
-            print!("\x1b[38;5;{0}mColor{0:03}\x1b[m ", color);
-            if color % 8 == 7 {
-                println!("");
-            }
-        }
-    }
+    // #[test]
+    // fn main() {
+    //     for color in 0..256 {
+    //         print!("\x1b[38;5;{0}mColor{0:03}\x1b[m ", color);
+    //         if color % 8 == 7 {
+    //             println!("");
+    //         }
+    //     }
+    // }
 }
