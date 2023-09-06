feeb936d5471583764339480c0c88012b27b6272
wuliao97 <wuliao.lw@gmail.com>
2023-09-07 08:22:03 +0900

    first commit

---
+++ src/lib.rs
@@ -0,0 +1,34 @@
+macro_rules! info {
+    ($target:expr) => (println!("\x1b[38;5;004m{}\x1b[m", $target))
+}
+
+macro_rules! warn {
+    ($target:expr) => (println!("\x1b[38;5;220m{}\x1b[m", $target))
+}
+
+macro_rules! error {
+    ($target:expr) => (println!("\x1b[38;5;124m{}\x1b[m", $target))
+}
+
+
+
+#[cfg(test)]
+mod tests {
+
+    #[test]
+    fn it_works() {
+        info!("Hello World");
+        warn!("Hello World");
+        error!("Hello World");
+    }
+
+    #[test]
+    fn main() {
+        for color in 0..256 {
+            print!("\x1b[38;5;{0}mColor{0:03}\x1b[m ", color);
+            if color % 8 == 7 {
+                println!("");
+            }
+        }
+    }
+}
